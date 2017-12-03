20a6bf0b0160195d0669fe2cccfc4e10ece139d1
AndrewD14 <Leingod2005@gmail.com>
2017-12-03 15:05:23 -0600

    Added a function to display to the results of grabbing all the products from the database.

--- js/customer.js
+++ js/customer.js
@@ -5,3 +5,23 @@ var Promises = require("bluebird");
 var products = null;
 var newOrder = [];
 
+//gets a list of products
+var displayProducts = function(connection){
+	return new Promises(function(resolve, reject){
+		connection.getProducts()
+		.then(function(results){
+			products = results;
+
+			if(products.length > 0){
+				console.log("Item Number\tItem\t\tPrice");
+				for(i in products)
+					console.log(products[i].itemId+"\t\t"+products[i].name+"\t\t"+products[i].price);
+			}
+			return resolve("Success");
+		})
+		.catch(function(error){
+			console.log("ERROR: "+error);
+			return reject("Failure");
+		});
+	});
+}
