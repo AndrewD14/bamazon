d2c84232b300a6c91994e7640562ca52861f3fc4
AndrewD14 <Leingod2005@gmail.com>
2017-12-03 14:51:50 -0600

    Added the function to get all the products.

--- js/database.js
+++ js/database.js
@@ -10,6 +10,33 @@ var pool = mysql.createPool({
   connectionLimit: 1
 });
 
+//gets a list of all products
+exports.getProducts = function(){
+	return new Promises(function(resolve, reject){
+		pool.getConnection(function(error, connection){
+			if(error) return reject(error);
+
+			connection.query('SELECT item_id, product_name, price, stock_quantity FROM products', function (error, results, fields){
+				if(error) return reject(error);
+
+				var products = [];
+
+				for(i in results){
+					products.push({
+						itemId: results[i].item_id,
+						name: results[i].product_name,
+						price: results[i].price,
+						stock: results[i].stock_quantity
+					});
+				}
+
+				connection.release();
+				return resolve(products);
+			});
+		});
+	});	
+}
+
 
 //closes the pool
 exports.closeConnection = function(){
