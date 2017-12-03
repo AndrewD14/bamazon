721a63c08129e1292278e10b922b4dfe7489119d
AndrewD14 <Leingod2005@gmail.com>
2017-12-03 14:52:28 -0600

    Added in the function to retrieve info for only 1 product.

--- js/database.js
+++ js/database.js
@@ -37,6 +37,33 @@ exports.getProducts = function(){
 	});	
 }
 
+//gets all info on a specific product
+exports.getSpecificProduct = function(id){
+	return new Promises(function(resolve, reject){
+		pool.getConnection(function(error, connection){
+			if(error) return reject(error);
+			
+			var sql = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE item_id = ?";
+			connection.query(mysql.format(sql, [id]), function (error, results, fields) {
+	  			if (error){
+	  				connection.release();
+	  				return reject(error);
+	  			};
+
+	  			connection.release();
+	  			if(results[0]){
+	  				return resolve({
+	  					itemId: results[0].item_id,
+						name: results[0].product_name,
+						price: results[0].price,
+						stock: results[0].stock_quantity
+	  				});
+	  			}
+	  			return resolve(null);
+	  		});
+		});
+	});
+}
 
 //closes the pool
 exports.closeConnection = function(){
