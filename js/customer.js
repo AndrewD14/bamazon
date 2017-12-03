51c9e9826b89c448d08250821dae5c523751321c
AndrewD14 <Leingod2005@gmail.com>
2017-12-03 15:06:37 -0600

    Added in the functions to deal with getting the inputs for placing an order.

--- js/customer.js
+++ js/customer.js
@@ -25,3 +25,62 @@ var displayProducts = function(connection){
 		});
 	});
 }
+
+//bunch of nested inquirer to have the customer place an order
+var placeOrder = function(connection, userId){
+	inquirer.prompt([
+		{
+			name: "selection",
+			message: "Enter the item ID of what you would like to buy, separated by a comma (ie: 1, 5, 9)"
+		}
+	]).then(function(answers){
+		var selection = answers.selection.split(",");
+
+		if(0 < selection.length)
+			getAmountToOrder(connection, 0, selection, userId);
+	});
+}
+
+//gets how many of each products the customer wants to order
+function getAmountToOrder(connection, index, selection, userId){
+	var id = parseInt(selection[index]);
+	if(id){
+		connection.getSpecificProduct(id)
+		.then(function(results){
+			if(results){
+				inquirer.prompt([
+					{
+						name: "amount",
+						type: "input",
+						message: "How many "+results.name+" would you like to buy?",
+						validate: validateForNumber
+					}
+				]).then(function(answer){
+					newOrder.push({id: id, name: results.name, amount: parseInt(answer.amount), price: results.price});
+
+					index++;
+					if(index < selection.length)
+						getAmountToOrder(connection, index, selection, userId);
+					else
+						insertOrder(connection, newOrder, userId);
+				});
+			}
+		})
+		.error(function(error){
+			console.log("Error retrieving product info: "+error);
+			index++;
+			if(index < selection.length)
+				getAmountToOrder(connection, index, selection, userId);
+			else
+				insertOrder(connection, newOrder, userId);
+		});
+	}
+	else{
+		console.log(selection[index]+" is not a valid item id. Skipping input.");
+		index++;
+		if(index < selection.length)
+			getAmountToOrder(connection, index, selection, userId);
+		else
+			insertOrder(connection, newOrder, userId);
+	}
+}
