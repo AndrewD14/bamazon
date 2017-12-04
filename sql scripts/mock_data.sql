279bb4bc558ce7926a37c7d387934d5a6c4dbfa9
AndrewD14 <Leingod2005@gmail.com>
2017-12-03 22:04:31 -0600

    Remove the auto increment on the role_id in the roles table. Updated the mock data file.

--- sql scripts/mock_data.sql
+++ sql scripts/mock_data.sql
@@ -7,14 +7,119 @@ INSERT INTO departments
 VALUES
 ('Foods', '', 12000);
 
+#Products
 INSERT INTO products
 (product_name,department_id,price,stock_quantity)
 VALUES
-("Banana", 1, 12.50, 10);
+("Banana", 1, .79, 15);
+
+INSERT INTO products
+(product_name,department_id,price,stock_quantity)
+VALUES
+("Frozen Pizza", 1, 4.99, 20);
+
+INSERT INTO products
+(product_name,department_id,price,stock_quantity)
+VALUES
+("Apple", 1, .69, 10);
+
+INSERT INTO products
+(product_name,department_id,price,stock_quantity)
+VALUES
+("Milk", 1, 2.49, 15);
+
+INSERT INTO products
+(product_name,department_id,price,stock_quantity)
+VALUES
+("Cheese", 1, 1.99, 12);
+
+#departments
+INSERT INTO departments
+(department_name,department_desc,over_head_cost)
+VALUES
+('Toys', '', 12000);
+
+#Products
+INSERT INTO products
+(product_name,department_id,price,stock_quantity)
+VALUES
+("Toy Truck", 2, 5.99, 15);
+
+INSERT INTO products
+(product_name,department_id,price,stock_quantity)
+VALUES
+("Nerf Gun", 1, 14.99, 10);
+
+INSERT INTO products
+(product_name,department_id,price,stock_quantity)
+VALUES
+("R/C Car", 1, 24.99, 10);
+
+INSERT INTO products
+(product_name,department_id,price,stock_quantity)
+VALUES
+("Stuff Bear", 1, 4.49, 15);
+
+INSERT INTO products
+(product_name,department_id,price,stock_quantity)
+VALUES
+("Star Wars Lego", 1, 19.99, 20);
 
 
 #user roles
 INSERT INTO roles
-(role_type, description)
+(role_id, role_type, description)
 VALUES
-('C', 'customer roles');
+(1, 'C', 'Customer');
+
+INSERT INTO roles
+(role_id, role_type, description)
+VALUES
+(2, 'M', 'Manager');
+
+INSERT INTO roles
+(role_id, role_type, description)
+VALUES
+(3, 'S', 'Supervisor');
+
+#user
+INSERT INTO users
+(login_id, f_name, l_name, password)
+VALUES
+('Manager', 'Andrew','Damico','1234');
+
+INSERT INTO users
+(login_id, f_name, l_name, password)
+VALUES
+('Customer1', 'Billy','Bob','1234');
+
+INSERT INTO users
+(login_id, f_name, l_name, password)
+VALUES
+('Supervisor', 'Sally','Sullivan','1234');
+
+#user_roles
+INSERT INTO user_role
+(user_id, role_id)
+VALUES
+(1, 1);
+
+INSERT INTO user_role
+(user_id, role_id)
+VALUES
+(1, 2);
+
+INSERT INTO user_role
+(user_id, role_id)
+VALUES
+(2, 1);
+
+INSERT INTO user_role
+(user_id, role_id)
+VALUES
+(3, 1);
+
+INSERT INTO user_role
+(user_id, role_id)
+VALUES
+(3, 3);
